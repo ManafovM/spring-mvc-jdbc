213653f2b2495814362ea94953642e491283abdd
Marif Manafov <manafov_m@bk.ru>
2021-08-26 17:58:29 +0600

    added model classes

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Car {
+    private long id;
+    private String model;
+    private User owner;
+}
